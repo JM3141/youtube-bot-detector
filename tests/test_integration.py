fc7e68ab85edef17491d0c7c7910485f3531b629
JM3141 <JalalMasembe@outlook.com>
2025-12-27 20:04:57 +0000

    test: add integration test for get_all_comment_for_videos

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,7 +4,10 @@ from pprint import pprint
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 from src.api_client import get_comment_threads
+from src.api_client import get_all_comments_for_videos
+from datetime import datetime
 
+#change this by adding the new attributes
 
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -34,15 +37,66 @@ def test_get_comment_threads_real():
    
     assert "author" in first
     assert "text" in first
-    assert "published" in first
+    assert "publishedAt" in first
     assert "likeCount" in first
+    assert "authorChannelId" in first
+    assert "authorProfileImageUrl" in first
+    assert "authorChannelUrl" in first
+    assert "updatedAt" in first
 
     assert isinstance(first["author"], str)
     assert isinstance(first["text"], str)
-    assert isinstance(first["published"], str)
+    assert isinstance(first["publishedAt"], datetime)
     assert isinstance(first["likeCount"], int)
-
+    assert isinstance(first["authorChannelId"],str)
+    assert isinstance(first["authorChannelUrl"],str)
+    assert isinstance(first["updatedAt"],datetime)
+    
     pprint(comments)
     #pprint formats complex python objects so they are easy to read
+
+@pytest.mark.integration
+def test_get_all_comments():
+
+    if not API_KEY:
+        pytest.skip("GOOGLE_API_KEY is not set in environment") 
+
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+    video_ids = ['AHRM_QNvp9E']
+
+    comments = get_all_comments_for_videos(youtube, video_ids)
+
+    assert isinstance(comments,dict)
+    assert len(comments) > 0
+
+    first = comments[video_ids[0]][0] 
+    #first_replies = comments[video_ids[0]][0]["replies"]
+
+    #assert len(first_replies) > 0
+
+    assert "author" in first
+    assert "text" in first
+    assert "publishedAt" in first
+    assert "likeCount" in first
+    assert "authorChannelId" in first
+    assert "authorProfileImageUrl" in first
+    assert "authorChannelUrl" in first
+    assert "updatedAt" in first
+
+    assert isinstance(first["author"], str)
+    assert isinstance(first["text"], str)
+    assert isinstance(first["publishedAt"], datetime)
+    assert isinstance(first["likeCount"], int)
+    assert isinstance(first["authorChannelId"],str)
+    assert isinstance(first["authorChannelUrl"],str)
+    assert isinstance(first["updatedAt"],datetime)
+
+    pprint(comments)
+
+
+
+
+
 
     
