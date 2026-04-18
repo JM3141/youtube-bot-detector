0549e8183358466cc79e91c63175920bf4c310e9
JM3141 <JalalMasembe@outlook.com>
2026-04-18 15:22:00 +0100

    create pipeline that builds one merged feature dictionary per comment

--- src/features/feature_pipeline.py
+++ src/features/feature_pipeline.py
@@ -1,11 +1,52 @@
 from .features import extract_text_features, extract_timing_features, extract_behavioural_features, extract_video_features 
 
 
-#pipeline produces two  dictionaries: one with per‑authorand one with per‑video metrics
+
+#takes youtube comment rows and transforms them into a 1 complete
+#dicitonary per comment
 def build_feature_pipeline(rows):
 
+   #group comments by author
    author_features = extract_behavioural_features(rows)
+
+   #group comments by video
    video_features = extract_video_features(rows)
+
+   final_dataset = []
+
+   for row in rows:
+       
+       text_feats = extract_text_features(row)
+
+       timing_feats = extract_timing_features(row)
+
+       author_feats = author_features[row["authorname"]]
+
+       video_feats = video_features[row["youtubevideo_id"]]
+
+
+       # ** means the items in a dictionary(key value pairs)
+       # are loaded as items in another dictionary
+
+       merged = {
+           **text_feats,
+           **timing_feats,
+           **author_feats,
+           **video_feats
+       }
+
+       final_dataset.append(merged)
+
+   return final_dataset    
+
+
+
+
+
+
+
+
+
 
    
 
