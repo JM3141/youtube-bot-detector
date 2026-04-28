eacec646e33ccb4766e7996d893850cc437c83dc
JM3141 <JalalMasembe@outlook.com>
2026-04-28 13:55:50 +0100

    add function to return the dataframe constructed

--- src/modeling/build_dataframe.py
+++ src/modeling/build_dataframe.py
@@ -9,7 +9,13 @@ rows = load_raw_comments()
 
 data = build_feature_pipeline(rows)
 
+#data frame is a 2d table like structure
+#data is arranged in rows and columns
 df = pd.DataFrame(data)
+
+def construct_dataframe():
+
+    return df
 
 
 
