7b8596c95b024073543e11171589c8b3bd8babda
JM3141 <JalalMasembe@outlook.com>
2026-05-03 13:06:06 +0100

    added isBot column to dataframe, set values to None

--- src/modeling/build_dataframe.py
+++ src/modeling/build_dataframe.py
@@ -2,7 +2,7 @@ import pandas as pd
 #pandas is used for data analysis and manipulation
 
 from ..db.extract_data import load_raw_comments
-from ..features import build_feature_pipeline
+from ..features.feature_pipeline import build_feature_pipeline
 
 
 rows = load_raw_comments()
@@ -12,10 +12,15 @@ data = build_feature_pipeline(rows)
 #data frame is a 2d table like structure
 #data is arranged in rows and columns
 df = pd.DataFrame(data)
+df["isBot"] = None
 
 def construct_dataframe():
 
+    
     return df
+    #acquiring first row of dataframe
+    #print(df.iloc[0])
+    
 
 
 
