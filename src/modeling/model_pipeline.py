235f64d9cf6a829246faf6573a9927076f94b37b
JM3141 <JalalMasembe@outlook.com>
2026-06-02 17:44:09 +0100

    updated load_unlabelled_data method to keep the first row(header) and skip to row 201

--- src/modeling/model_pipeline.py
+++ src/modeling/model_pipeline.py
@@ -45,9 +45,10 @@ def evaluating_model(X_test, y_test, model):
 
 #function to load the remaining unlabelled data
 def load_unlabelled_data(scaler):
-
+    
+    #updated method to keep the first row(header)
     #skip 201 rows to load the unlabelled data
-    dataframe = pd.read_csv("datasets/labeled.csv", skiprows = 201)
+    dataframe = pd.read_csv("datasets/labeled.csv", skiprows = range(1, 200))
 
     #From unlabelled rows, extract only the input features
     X_unlabelled = dataframe[['char_length', 'word_count',
@@ -73,6 +74,8 @@ def predict_unlabelled(model, X_unlabelled_scaled):
     y_probability = model.predict_proba(X_unlabelled_scaled)[:,1]
 
     return y_predict, y_probability
+
+
 
 
 
